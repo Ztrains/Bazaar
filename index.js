b65d8c28566d36cde02b6d719f39f9df633f2c08
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-02-02 03:21:33 -0500

    quick formats

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
 	return res.send("Welcome to Bazaar!");
-})
+});
 
 var port = process.env.PORT || 8000;
-app.listen(port, function() {
+app.listen(port, () => {
 	console.log("Running server on port " + port);
 });
