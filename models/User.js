3777560af7ad5c846e80b80679fbe463e1196663
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-02-10 15:03:40 -0500

    consistent formatting across all backend files; remove redundant gitignores

--- models/User.js
+++ models/User.js
@@ -5,6 +5,6 @@ var UserSchema = new mongoose.Schema({
     password: String, //not sure if needed cause google log in?
     email: String,
     updatedAt: {type: Date, default: Date.now}
-})
+});
 
 module.exports = mongoose.model('User', UserSchema);
