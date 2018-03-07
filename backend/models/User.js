99f002ba66e303de842102e9b57d51bc214390c8
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-03-06 22:36:37 -0500

    new sign up code

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,13 +3,14 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
 	name: String,
     username: String,
-    password: String, //not sure if needed cause google log in?
+    //password: String, //not sure if needed cause google log in?
     email: String,
     updatedAt: {type: Date, default: Date.now},
     googleId: String,
     savedRecipes: [Number],
     preferences: [String],
-    token: String
+    token: String,
+    imageUrl: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
