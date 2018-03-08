21523c0e3df07fff798787dfb5a17b5a064827bf
InfernalVortex <pranz10@gmail.com>
2018-03-08 16:51:12 -0500

    template for calendar

--- frontend/bazaar/src/calendar.js
+++ frontend/bazaar/src/calendar.js
@@ -15,11 +15,72 @@ export default class Calendar extends React.Component {
   render() {
     return(
       <div>
-        <br/>
-        <br/>
-        <br/>
-
-
+      <table class="table">
+      <thead class="thead-dark">
+      <tr>
+        <th scope="col">Meal</th>
+        <th scope="col">Monday</th>
+        <th scope="col">Tuesday</th>
+        <th scope="col">Wednesday</th>
+        <th scope="col">Thursday</th>
+        <th scope="col">Friday</th>
+        <th scope="col">Saturday</th>
+        <th scope="col">Saturday</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th scope="row">Breakfast</th>
+        <td>Jelly</td>
+        <td>Pudding</td>
+        <td>Bread</td>
+        <td>Ramen</td>
+        <td>Eggs</td>
+        <td>Cory Laker</td>
+        <td>More Food</td>
+      </tr>
+      <tr>
+        <th scope="row">Lunch</th>
+        <td>Jelly</td>
+        <td>Pudding</td>
+        <td>Bread</td>
+        <td>Ramen</td>
+        <td>Eggs</td>
+        <td>Cory Laker</td>
+        <td>More Food</td>
+      </tr>
+      <tr>
+        <th scope="row">Snacks</th>
+        <td>Jelly</td>
+        <td>Pudding</td>
+        <td>Bread</td>
+        <td>Ramen</td>
+        <td>Eggs</td>
+        <td>Cory Laker</td>
+        <td>More Food</td>
+      </tr>
+      <tr>
+        <th scope="row">Dinner</th>
+        <td>Jelly</td>
+        <td>Pudding</td>
+        <td>Bread</td>
+        <td>Ramen</td>
+        <td>Eggs</td>
+        <td>Cory Laker</td>
+        <td>More Food</td>
+      </tr>
+      <tr class="table-info">
+        <th scope="row">Total Calories: </th>
+        <td>2000</td>
+        <td>2000</td>
+        <td>2000</td>
+        <td>2000</td>
+        <td>2000</td>
+        <td>2000</td>
+        <td>2000</td>
+      </tr>
+      </tbody>
+      </table>
       </div>
     );
   }
