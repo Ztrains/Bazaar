23def56307c7166dfcedbd2833655995f2a77b78
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-03-08 00:43:36 -0500

    add route for creating new recipes

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 
 var RecipeSchema = new mongoose.Schema({
     name: String,
-    ingredients: [String],
+    description: String,
+    ingredients: [Object],
+    steps: [Object],
+    calories: String,
+    servingSize: String,
+    tags: [String],
+    createdBy: String,
     updatedAt: {type: Date, default: Date.now}
 });
 
