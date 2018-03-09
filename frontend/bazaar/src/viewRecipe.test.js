892e44b1372d6ffffef9d6cfa16645a9863ec64b
myers259 <myers259@purdue.edu>
2018-03-09 18:43:29 -0500

    adding stuff for test. Possible final testing commit

--- frontend/bazaar/src/viewRecipe.test.js
+++ frontend/bazaar/src/viewRecipe.test.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
-
+import { expect } from 'chai';
 import ViewRecipe from './viewRecipe';
+import localStorage from 'mock-local-storage';
+
+beforeEach(() => {
+    global.window = {};
+    window.localStorage = global.localStorage;
+}); 
+
+
+it('Checking to make sure it  renders properly', () => {
+    const wrapper = shallow(<ViewRecipe/>);
+});
+
+/*
+
+it('Checking comment box', () => {
+    const wrapper = shallow(<ViewRecipe/>);
+    
+});
+
+
+it('Selecting day and time', () => {
+    const wrapper = shallow(<ViewRecipe/>);
+    wrapper.find('#timeVal').simulate('change', {target: {value: 'Lunch'}});
+    wrapper.find('#dayVal').simulate('change', {target: {value: 'Monday'}});
+    expect(wrapper.state().dayValue).equal('Monday');
+    expect(wrapper.state().timeValue).equal('Lunch');
+});
 
 it('Checking to make sure the upvote/downvote button changes are properly shwon', () => {
     //Add the proper finding once the upvote/downvote number is added
@@ -10,4 +37,4 @@ it('Checking to make sure the upvote/downvote button changes are properly shwon'
     wrapper.find('').simulate('change', {target: {value: '69'}});
     expect(wrapper.find('').props().value).to.equal('69');
     console.log(wrapper.find('').props().value);
-});
+});*/
