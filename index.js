3e6f8969578f2ed1545214b39e4e4a6e17daaf53
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-03-08 22:32:50 -0500

    fixed recipe find by id code

--- index.js
+++ index.js
@@ -389,6 +389,7 @@ app.post("/recipes/save", (req, res) => {
 app.post("/recipes/:id", (req, res) => {
 	let token = req.body.accessToken;
 	let usrname = req.body.username;
+	let currentUser;
 
 	if (!req.params.id) {
 		return res.status(400).json({message: "Missing recipe ID"});
@@ -402,20 +403,26 @@ app.post("/recipes/:id", (req, res) => {
 			return res.status(400).json({message: "No user found"});
 		}
 
-		Recipe.findOne({_id: req.params.id}, (err, recipe) => {
+		currentUser = user;
+	});
+
+	Recipe.findOne({_id: req.params.id}, (err, recipe) => {
 			if (err) {
 				return res.status(500).json({message: "Internal server error"});
 			}
 			if (!recipe) {
 				return res.status(400).json({message: "No recipe found"});
 			}
-			console.log(recipe);
 			
-			var dishData = ml.formatDishData(recipe.calories, recipe.servingSize, recipe.upvotes, recipe.steps, recipe.tags);
-			var prediction = ml.predict(user.mlDishData, user.mlDishRatings, dishData);
+			console.log(recipe);
+			if (currentUser) {
+				var dishData = ml.formatDishData(recipe.calories, recipe.servingSize, recipe.upvotes, recipe.steps, recipe.tags);
+				var prediction = ml.predict(user.mlDishData, user.mlDishRatings, dishData);
+				
+				return res.status(200).json({message: "Success", data: recipe, ml: prediction});
+			}
 
-			return res.status(200).json({message: "Success", data: recipe, ml: prediction});
-		});
+			return res.status(200).json({message: "Success", data: recipe});
 	});
 });
 
