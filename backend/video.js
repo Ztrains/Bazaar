9a144fcd9cacc797b83702af4c0931512997727a
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-03-09 15:47:38 -0500

    boi come on

--- backend/video.js
+++ backend/video.js
@@ -6,22 +6,23 @@ function getVideos(query, next) {
     var searchTerm = query.replace(" ", "+");
     var url = "https://www.youtube.com/results?search_query=" + searchTerm
     
-    var data = request(url, function(err, resp, body) {
+    // var dat;
+    request(url, function(err, resp, body) {
       $ = cheerio.load(body);
       links = $('a'); //jquery get all hyperlinks
       $(links).each(function(i, link) {
         var videoLink = String($(link).attr("href"));
         if (videoLink.search("/watch") !== -1 && videoLink.search("&list=") === -1) {
             var retData = videoLink.replace("/watch?v=", "");
-            return retData;
+            next(retData);
         }
       });
 
     return "";
     });
 
-    console.log(data);
-    // next(data);
+    // console.log(data);
+    
 }
 
 module.exports = { getVideos };
