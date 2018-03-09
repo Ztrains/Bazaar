a30db5c18bdf04584e6961c63057989897d1de57
Abir <abirshukla@gmail.com>
2018-03-08 20:38:00 -0500

    Added format function for ml

--- ML Playground/ml.js
+++ ML Playground/ml.js
@@ -7,6 +7,21 @@ function predict(userData,userRatings,dishData) {
 	return knn.predict(dishData);
 }
 
+function formatDishData(calories,servingSize,upvotes,steps,tags) {
+	var dishData = [upvotes,parseInt(calories),parseInt(servingSize),steps.length];
+	var tagOptions = ["Vegetarian","Vegan","Gluten-Free","Lactose-Free","Low Carb","Paleo"];
+	for (var i = 0; i < tagOptions.length; i++) {
+		if (tags.indexOf(tagOptions[i]) >= 0) {
+			dishData.push(1);
+		}
+		else {
+			dishData.push(0);
+		}
+	}
+
+
+}
+
 //userPref holds recipe data that user rated
 var userPref = [[0, 0, 0], [0, 1, 1], [1, 1, 0], [2, 2, 2], [1, 2, 2], [2, 1, 2]];
 //userRatings hold what user rated the aformentioned recipes
